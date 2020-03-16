89ee0bebae002da2063140020173bbbd316dfc8c
dayran <dayran1984@outlook.com>
2020-03-15 22:51:53 -0400

    changed from folder test-2

--- Test1Console/Test1Console/Program.cs
+++ Test1Console/Test1Console/Program.cs
@@ -6,6 +6,7 @@ namespace Test1Console
     {
         static void Main(string[] args)
         {
+            /*other repository changed that*/
             var name = "Jonh";
             Console.WriteLine($"Hello {name}");
             Console.ReadLine();
